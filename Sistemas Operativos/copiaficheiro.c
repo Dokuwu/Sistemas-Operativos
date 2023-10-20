c1b606eeb0df173039af2c6568c151ebab317244
Fabio Miranda <120479442+Dokuwu@users.noreply.github.com>
2023-10-20 11:25:38 +0100

    Update copiaficheiro.c

--- Sistemas Operativos/copiaficheiro.c
+++ Sistemas Operativos/copiaficheiro.c
@@ -1,1 +1,33 @@
-#include <fcntl.h>#include <unistd.h>int main(int argc, char* argv[]) {	int fd, copia, leitura;	char buffer[200];	copia = open("ficheiro_copia.txt", O_WRONLY);	if (copia == -1) {		write(STDERR_FILENO, "Ficheiro de copia n„o existe, foi criado no diretorio.\n", 55);		copia = open("ficheiro_copia.txt", O_CREAT, S_IRUSR | S_IWUSR);	}	for (int i = 1; i < argc; i++) {		fd = open(argv[i], O_RDONLY);		if (fd == -1) write(STDERR_FILENO, "ficheiro nao existe\n", 20);		while ((leitura = read(fd, buffer, 200)) > 0) {			write(copia, buffer, leitura);		}		if (leitura == -1) write(STDERR_FILENO, "Erro na leitura\n", 16);	}	close(fd);	close(copia);	return 0;}
+#include <fcntl.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	int fd, copia, leitura;
+	char buffer[200];
+///O OBJETIVO ERA PARA CRIAR POR EXEMPLO OLA.COPIA E NAO FICHEIRO.COPIA
+  
+	copia = open("ficheiro_copia.txt", O_WRONLY);
+
+	if (copia == -1) {
+		write(STDERR_FILENO, "Ficheiro de copia n√£o existe, foi criado no diretorio.\n", 55);
+		copia = open("ficheiro_copia.txt", O_CREAT, S_IRUSR | S_IWUSR);
+	}
+
+	for (int i = 1; i < argc; i++) {
+		fd = open(argv[i], O_RDONLY);
+
+		if (fd == -1) write(STDERR_FILENO, "ficheiro nao existe\n", 20);
+
+		while ((leitura = read(fd, buffer, 200)) > 0) {
+			write(copia, buffer, leitura);
+		}
+
+		if (leitura == -1) write(STDERR_FILENO, "Erro na leitura\n", 16);
+
+	}
+
+	close(fd);
+	close(copia);
+
+	return 0;
+}
